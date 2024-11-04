a097d5f052f7db17e10d612308048866883e37ec
helouazizi <ouazizi2028@gmail.com>
2024-11-04 14:27:58 +0100

    add somr

--- learn/main.c
+++ learn/main.c
@@ -1,30 +1,26 @@
-// learn/main.c
-// learn/main.c
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-int main() {
-    // Open a file using filestream
-    FILE *file = fopen("example.txt", "w");
-    if (file == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
-
-    // Write to the file
-    const char *text = "Hello, World!\n";
-    size_t bytes_written = fwrite(text, sizeof(char), strlen(text), file);
-    if (bytes_written < strlen(text)) {
-        perror("Error writing to file");
-        fclose(file);
-        return 1;
-    }
-
-    // Close the filestream
-    if (fclose(file) != 0) {
-        perror("Error closing file");
-        return 1;
-    }
-
-    return 0;
+int check_authentication(char *password) {
+char password_buffer[16];
+ int auth_flag = 0;
+ strcpy(password_buffer, password);
+ if(strcmp(password_buffer, "brillig") == 0)
+ auth_flag = 1;
+ if(strcmp(password_buffer, "outgrabe") == 0)
+ auth_flag = 1;
+ return auth_flag;
+}
+int main(int argc, char *argv[]) {
+ if(argc < 2) {
+ printf("Usage: %s <password>\n", argv[0]);
+ exit(0);
+ }
+ if(check_authentication(argv[1])) {
+ printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+ printf(" Access Granted.\n");
+ printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+ } else {
+ printf("\nAccess Denied.\n");
+ }
 }
