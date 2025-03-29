10634ff8ec2b86d654cd063bcdb137b13f166a09
helouazizi <ouazizi2code@gmail.com>
2025-03-29 16:02:56 +0000

    add loaddtat

--- repository/repo.go
+++ repository/repo.go
@@ -1,9 +1,19 @@
 package repository
 
 import (
+	"encoding/json"
+	"fmt"
 	"groupie-tracker/models"
+	"io"
+	"log"
+	"net/http"
 	"sync"
 )
+
+// "artists": "https://groupietrackers.herokuapp.com/api/artists",
+// "locations": "https://groupietrackers.herokuapp.com/api/locations",
+// "dates": "https://groupietrackers.herokuapp.com/api/dates",
+// "relation": "https://groupietrackers.herokuapp.com/api/relation"
 
 type Store struct {
 	Artists []models.Artist
@@ -14,4 +24,24 @@ func New_Store() *Store {
 	return &Store{}
 }
 
+// loadd data
+func (s *Store) LoadData() {
+	// lets request the api to get artist data
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
 
+	content, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Unmarshal JSON into the struct
+	if err = json.Unmarshal(content, &s.Artists); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(s.Artists[0])
+}
