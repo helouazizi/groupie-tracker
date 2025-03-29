9afa22b7cf8943ba3f54f090ee8354c1a05acb19
helouazizi <ouazizi2code@gmail.com>
2025-03-29 16:30:29 +0000

    add router

--- router/routes.go
+++ router/routes.go
@@ -1,1 +1,14 @@
 package router
+
+import (
+	"groupie-tracker/handlers"
+	"groupie-tracker/repository"
+	"net/http"
+)
+
+func NewRouter(s *repository.Store) *http.ServeMux {
+	mux := http.NewServeMux()
+	toHandle := &handlers.Home_handler{Store: s}
+	mux.HandleFunc("/", toHandle.Home)
+	return mux
+}
