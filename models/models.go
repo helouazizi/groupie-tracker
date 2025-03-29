455b7c08f578515224e55dc1f805c42b00e9dc15
helouazizi <ouazizi2code@gmail.com>
2025-03-29 15:47:26 +0000

    add artist

--- models/models.go
+++ models/models.go
@@ -1,2 +1,25 @@
 package models
 
+type Artist struct {
+	ID           int      `json:"id"`
+	Name         string   `json:"name"`
+	Image        string   `json:"image"`
+	// Members      []string `json:"members"`
+	// FirstAlbum   string   `json:"firstAlbum"`
+	// CreationYear int      `json:"creationYear"`
+}
+
+// type Location struct {
+// 	ID        int      `json:"id"`
+// 	Locations []string `json:"locations"`
+// }
+
+// type Date struct {
+// 	ID    int      `json:"id"`
+// 	Dates []string `json:"dates"`
+// }
+
+// type Relation struct {
+// 	ID       int                 `json:"id"`
+// 	Relation map[string][]string `json:"relation"`
+// }
