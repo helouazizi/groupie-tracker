857b5e15ee8818cfbb902949e8872d80fe5b94d5
helouazizi <ouazizi2code@gmail.com>
2025-03-31 17:28:23 +0000

    add locations

--- handlers/artist_details_handler.go
+++ handlers/artist_details_handler.go
@@ -5,7 +5,6 @@ import (
 	"groupie-tracker/models"
 	"groupie-tracker/repository"
 	"net/http"
-	"sync"
 )
 
 type ArtistDeatils struct {
@@ -13,6 +12,8 @@ type ArtistDeatils struct {
 }
 
 func (h *ArtistDeatils) ArtistDetail(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*") // Fix CORS issues
 	// Extract ID from query parameters
 	id := r.URL.Query().Get("id")
 	if id == "" {
@@ -26,11 +27,11 @@ func (h *ArtistDeatils) ArtistDetail(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Artist not found", http.StatusNotFound)
 		return
 	}
-
-	// Define variables to hold fetched data
-	var locationData, concertData, relationData interface{}
-	var wg sync.WaitGroup
-	errChan := make(chan error, 3)
+	lovations, exist := h.Store.GetLocationById(id)
+	if !exist {
+		http.Error(w, "Locations not found", http.StatusNotFound)
+		return
+	}
 
 	// Function to fetch data
 	// fetchData := func(url string, target interface{}) {
@@ -42,27 +43,17 @@ func (h *ArtistDeatils) ArtistDetail(w http.ResponseWriter, r *http.Request) {
 	// }
 
 	// Concurrently fetch data
-	wg.Add(3)
+	//wg.Add(3)
 	// go fetchData(artist.Locations, &locationData)
 	// go fetchData(artist.ConcertDates, &concertData)
 	// go fetchData(artist.Relations, &relationData)
 
-	// Wait for all fetch operations to complete
-	wg.Wait()
-	close(errChan)
-
-	// Check if any error occurred
-	if len(errChan) > 0 {
-		http.Error(w, "Failed to fetch artist details", http.StatusInternalServerError)
-		return
-	}
-
 	// Combine artist and fetched data into a response
 	extendedArtist := struct {
-		Artist   models.Artist `json:"artist"`
-		Location interface{}   `json:"locationData"`
-		Concert  interface{}   `json:"concertData"`
-		Relation interface{}   `json:"relationData"`
+		Artist   models.Artist   //`json:"artist"`
+		Location models.Location // `json:"locationData"`
+		Concert  models.Date     //`json:"concertData"`
+		Relation models.Relation //`json:"relationData"`
 	}{
 		Artist:   artist,
 		Location: locationData,
@@ -71,8 +62,6 @@ func (h *ArtistDeatils) ArtistDetail(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Respond with the extended artist JSON data
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*") // Fix CORS issues
 
 	if err := json.NewEncoder(w).Encode(extendedArtist); err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
