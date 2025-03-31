91cc56711e5273e9aba799f682b8baaee0fdfee8
helouazizi <ouazizi2code@gmail.com>
2025-03-31 03:04:19 +0000

    modify the router

--- router/routes.go
+++ router/routes.go
@@ -9,8 +9,8 @@ import (
 func NewRouter(s *repository.Store) *http.ServeMux {
 	mux := http.NewServeMux()
 	all := &handlers.Home_handler{Store: s}
-	deatils := &handlers.ArtistDeatils{Store: s}
+	//deatils := &handlers.ArtistDeatils{Store: s}
 	mux.HandleFunc("/", all.Home)
-	mux.HandleFunc("/artist", deatils.ArtistDetail)
+	//mux.HandleFunc("/artist", deatils.ArtistDetail)
 	return mux
 }
