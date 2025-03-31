ca3d7bf36d5c0eae8091fd506d87727f4bf2bc44
helouazizi <ouazizi2code@gmail.com>
2025-03-31 03:01:48 +0000

    modify the home handlers

--- handlers/home_handler.go
+++ handlers/home_handler.go
@@ -14,7 +14,7 @@ func (h *Home_handler) Home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*") // Fix CORS issues
 	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	if r.URL.Path != "/" {
@@ -22,9 +22,8 @@ func (h *Home_handler) Home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	all_artist := h.Store.GetArtists()
-
-	//artists := h.Store.GetArtists() // Ensure this returns data
 	if err := json.NewEncoder(w).Encode(all_artist); err != nil {
 		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
 	}
 }
