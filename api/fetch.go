6643776df7b0b9ecd74af203d5452f7539228865
helouazizi <ouazizi2code@gmail.com>
2025-03-31 01:50:33 +0000

    modify the fetch function

--- api/fetch.go
+++ api/fetch.go
@@ -2,21 +2,35 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"sync"
 )
 
 // FetchJSON fetches JSON from a given URL
-func Fetch(url string, target any) error {
+func Fetch(url string, target interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
 	resp, err := http.Get(url)
 	if err != nil {
-		return err
+		log.Printf("Error fetching data from %s: %v", url, err)
+		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to fetch: %s", url)
+		log.Printf("Failed to fetch data from %s: StatusCode %d", url, resp.StatusCode)
+		return
 	}
-
-	return json.NewDecoder(resp.Body).Decode(target)
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading response body from %s: %v", url, err)
+		return
+	}
+	// Parse the JSON response into the appropriate struct
+	if err := json.Unmarshal(body, &target); err != nil {
+		log.Printf("Error unmarshalling data from %s: %v", url, err)
+		return
+	}
 }
