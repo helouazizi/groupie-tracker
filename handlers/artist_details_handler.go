57d2b719b8ba6455ea39f16051cabf2c17059b28
helouazizi <ouazizi2code@gmail.com>
2025-03-31 17:39:14 +0000

    enhance the details function

--- handlers/artist_details_handler.go
+++ handlers/artist_details_handler.go
@@ -42,22 +42,6 @@ func (h *ArtistDeatils) ArtistDetail(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Locations not found", http.StatusNotFound)
 		return
 	}
-
-	// Function to fetch data
-	// fetchData := func(url string, target interface{}) {
-	// 	defer wg.Done()
-	// 	if err := api.Fetch(url, target); err != nil {
-	// 		log.Println("Error fetching data:", err)
-	// 		errChan <- err
-	// 	}
-	// }
-
-	// Concurrently fetch data
-	//wg.Add(3)
-	// go fetchData(artist.Locations, &locationData)
-	// go fetchData(artist.ConcertDates, &concertData)
-	// go fetchData(artist.Relations, &relationData)
-
 	// Combine artist and fetched data into a response
 	extendedArtist := struct {
 		Artist   models.Artist
@@ -70,9 +54,6 @@ func (h *ArtistDeatils) ArtistDetail(w http.ResponseWriter, r *http.Request) {
 		Dates:    dates,
 		Relations: relations,
 	}
-
-	// Respond with the extended artist JSON data
-
 	if err := json.NewEncoder(w).Encode(extendedArtist); err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
