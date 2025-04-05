97693a8195ad8b1a199fdc0438c0cc8f450c6137
helouazizi <ouazizi2code@gmail.com>
2025-04-05 22:53:01 +0000

    add data form

--- handlers/filter_artists.go
+++ handlers/filter_artists.go
@@ -2,11 +2,19 @@ package handlers
 
 import "groupie-tracker/repository"
 
+type FilterRequest struct {
+	CreationFrom int      `json:"creationFrom"`
+	CreationTo   int      `json:"creationTo"`
+	AlbumFrom    int      `json:"albumFrom"`
+	AlbumTo      int      `json:"albumTo"`
+	Members      int      `json:"members"`
+	Locations    []string `json:"locations"`
+}
 
 type Filter_Handler struct {
 	Store *repository.Store
 }
 
-func (f *Filter_Handler) Filter()  {
-	
+func (f *Filter_Handler) Filter() {
+
 }
